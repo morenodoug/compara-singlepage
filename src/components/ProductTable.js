0c00010bef5ebe95222cf50605678da557f77e7b
Douglas Moreno <morenodoug@gmail.com>
2017-09-06 14:28:54 -0300

    scroll deproductos

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -9,7 +9,7 @@ class ProductTable extends Component {
     }
 
     render(){
-
+        //agregar nombre de la compañia e imagen a cada oferta
         let offersWithImage = this.props.companies.map((company) => {
             return company.offers.map((offer) => {
                 offer.imageCompanyUrl = company.imageUrl;
@@ -19,17 +19,26 @@ class ProductTable extends Component {
         });
         let flatOffersWithImage = [].concat.apply([], offersWithImage);
 
-        let offerRows = flatOffersWithImage.map((offer) => (<OfferRow info={offer}/>))
+        //generar cara offerRow
+        let offerRows = flatOffersWithImage.map((offer) => (<OfferRow key={offer.id} info={offer}/>))
         console.log(flatOffersWithImage);
         console.log(offerRows);
+
+        let showlength = (flatOffersWithImage.length == 1) ? 
+                        `${flatOffersWithImage.length} result` :
+                        `${flatOffersWithImage.length} results`;
+
         return (
             <div>
                 <div > 
+                    <div className="row">
+                        <div className="col-md-6"><h3>{showlength} </h3></div>
+                        <div className="col-md-6">adasd</div>
+                    </div>
 
                 </div>
-                <div >
+                <div className="offer-list">
                     {offerRows}
-
 
                 </div>
             </div>
